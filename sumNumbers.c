2ebd2bc825ab0758ec9ead4f02958c54c44fb8c7
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2024-01-12 20:10:36 +0530

    Create sumNumbers.c

---
+++ sumNumbers.c
@@ -0,0 +1,10 @@
+#include <conio.h>
+#include <stdio.h>
+void main(){
+	int a,b,sum;
+	printf("Enter the two numbers to be added:");
+	scanf("%d %d",&a,&b);
+	sum=a+b;
+	printf("The sum of the two numbers is:%d",sum);
+	getch();
+}
