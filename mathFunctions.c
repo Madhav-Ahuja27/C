357b1ac6cc4a6f9edd1b9577f216b468540ace49
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2024-01-12 20:15:49 +0530

    Create mathFunctions.c

---
+++ mathFunctions.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+	printf("Enter two numbers:");
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("\nThe addition is:%d",a + b);
+	printf("\nThe subtraction is:%d",a - b);
+	printf("\nThe mutiplication is:%d",a * b);
+	printf("\nThe division is:%d",a / b);
+}
