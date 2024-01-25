7ceea1f497a1fde32db03a85b2bf28fbae6a7a03
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2024-01-25 23:31:20 +0530

    Create maxInt.c

---
+++ maxInt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+void main(){
+
+//	char x=0;
+//	int y=5;
+//	while(y==5){
+//		if(x<0){
+//			break;
+//		}
+//		x+=1;
+//	}
+//	printf("%d",x);
+
+
+//	int n;
+//	scanf("%d",&n);
+//	int b = n*8;
+//	int res = (pow(2,b-1))-1;
+//	int final = (pow(2,res))-1;
+//	printf("%d\n",final);
+	
+//	printf("%d",INT_MAX);
+	
+//	char ch= 0b11011101;
+
+//	printf("%d",~ch);
+
+//	printf("%d",9&&8);
+}
